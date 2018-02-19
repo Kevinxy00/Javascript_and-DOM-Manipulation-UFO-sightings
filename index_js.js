defdf276841a5b9c699d80e2dfe0155b058f0760
KY <kevinxy00@gmail.com>
2018-02-19 00:02:19 -0500

    added scrolling for long paginations

--- index_js.js
+++ index_js.js
@@ -17,8 +17,8 @@ var $showAll_button = document.querySelector("#show_All");
     // add eventListener to search button and show all button when clicked
 $search_button.addEventListener("click", search_multi);
 $showAll_button.addEventListener("click", show_all);
-    // selecting page progress row for calculating page progress (page # out of #);
-var $page_progress = document.querySelector("#page_progress");
+    // selecting all page progress rows for calculating page progress (page # out of #);
+var $page_progress = document.querySelectorAll("#page_progress");
 
     // defaul variables
 var page = 1;
@@ -97,6 +97,17 @@ function render_pagination(some_data, perPageCt=50, page_num=1) { // parameters:
     while($result_page.firstChild){
         $result_page.removeChild($result_page.firstChild);
     }
+
+    // set up button for previous page
+    previous_page = document.createElement('button');
+    previous_page.innerText = "Previous";
+    previous_page.setAttribute("class", ".btn-danger");
+    previous_page.setAttribute("id", "previous");
+    // execute function with a click
+    previous_page.addEventListener("click", function (){
+
+    });
+
      // get total # of pages needed to encompass all data.
     var last_page = Math.ceil(some_data.length / perPageCt); // rounds up the resulting number;
    
@@ -104,17 +115,20 @@ function render_pagination(some_data, perPageCt=50, page_num=1) { // parameters:
         var newPageLink = document.createElement('button');
         var currentPageNum = i + 1;
         newPageLink.innerText = currentPageNum;
-        newPageLink.setAttribute("class", "page_button");
+        newPageLink.setAttribute("class", "btn btn-outline-danger");
         newPageLink.setAttribute("id", currentPageNum); // set id at the current page number to easily get page number
-        // add event listener for each button
-        newPageLink.addEventListener("click", function (){ // to pass multiple functions with custom params
+    //  add event listener for each button
+        newPageLink.addEventListener("click", function (){ // on click, execute custom function
             var current_button = this.id; // get the current ID, which is the button/page number
             console.log("button clicked: " + current_button);
             slice_data(some_data=some_data, perPageCt, page_num=current_button);
             calc_page_progress(current_page=current_button, end_page=last_page);
+            window.scrollTo(0, 0); // scroll to the top of the window after rendering new table;
         });
+
         $result_page.appendChild(newPageLink); // append new pagination under the pagination <ul> 
     }   
+
     calc_page_progress(current_page=1, end_page=last_page); // renders the page progress for the first page
 }
 
@@ -132,12 +146,16 @@ function slice_data(some_data, perPageCt=50, page_num=1) { // parameters: object
 
 // calculate the page progress (page # out of ###). Called by render_pagination()
 function calc_page_progress(current_page, end_page) {
-    // clear the previous page progress
-    $page_progress.removeChild($page_progress.firstChild);
-    // create new paragraph element and let reader know what page they're on
-    var progressElem = document.createElement("p");
-    progressElem.innerHTML = `Page <b>${current_page}</b> out of <b>${end_page}</b>`
-    $page_progress.appendChild(progressElem);
+    // get page progress for each id="page_progress", before & after the alien sightings table; 
+    for (var i=0; i<$page_progress.length; i++){
+        // clear the previous page progress
+        $page_progress[i].removeChild($page_progress[i].firstChild);
+        // create new paragraph element and let reader know what page they're on
+        var progressElem = document.createElement("p");
+        progressElem.innerHTML = `Page <b>${current_page}</b> out of <b>${end_page}</b>`
+        $page_progress[i].appendChild(progressElem);
+    }
+
 }
 
 function render_table(some_data){
@@ -179,3 +197,12 @@ function render_table(some_data){
 $(document).ready(function() {
     $('#main').DataTable();
 } ); */
+
+// scroll up when clicking on footer banner text
+var $botToTop = document.querySelector("#to_top");
+$botToTop.addEventListener("click", function() {
+    window.scrollTo(0, 0);
+});
+
+// Render the first 100 entries from dataSet when first enter page:
+slice_data(dataSet, perPageCt=100, page_num=1)
